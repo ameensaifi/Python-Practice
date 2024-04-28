df5b32f715abd273f2e0cbea1afc9702a55170a8
Ameen Saifi <166974011+ameensaifi@users.noreply.github.com>
2024-04-28 10:45:46 +0530

    Largest Num from 3 Digit.py

--- Largest Num from 3 Digit.py
+++ Largest Num from 3 Digit.py
@@ -8,14 +8,10 @@ if num_1 > num_2:
         print("Largest Num is",num_1)
     elif num_3 > num_2:
         print("Largest Num is",num_3)
-    else:
-        print("")
 elif num_2 > num_1:
     if  num_2 > num_3:
         print("Larget Num is", num_2)
     elif num_3 > num_2:
         print("Largest Num is", num_3)
-    else:
-        print("")
 else:
     print("Largest Num is", num_3)
