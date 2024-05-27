4fdc9b82b764936220edf0f67d9b3fc138b54457
Ameen Saifi <166974011+ameensaifi@users.noreply.github.com>
2024-05-27 09:59:28 +0530

    Perfect Guess Game.py

--- Perfect Guess Game.py
+++ Perfect Guess Game.py
@@ -1,7 +1,7 @@
 #Perfect Guess Game
 import random
 name = input('Write your Name : ')
-print('***Welcome to the Game of Perfect Guess',name,'***')
+print('*** Welcome to the Game of Perfect Guess',name,'***')
 
 a = random.randint(1,100)
 x = 0
@@ -9,12 +9,11 @@ Input = int(input('Write down a number between 1 to 100 : '))
 while True:
     x+=1
     if Input == a:
-        print('Perfect Match')
+        print('Perfect Match!')
         break
+    elif Input > a:
+        print('A Smaller Number')
     else:
-        if Input > a:
-            print('A Smaller Number')
-        else:
-            print('A Bigger Number')
-        Input = int(input('Try again: '))
+        print('A Bigger Number')
+    Input = int(input('Try again: '))
 print(f'You guessed the correct number in {x} gusses')
